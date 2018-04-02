0dbee34f8ec0fc16950121b66de5fdf26e9ff2e9
Joseph <josephodr@hotmail.com>
2018-04-02 14:12:34 +0100

    User can now input dates to search for. Cleaned up a bit

--- company.py
+++ company.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import plotly
 import plotly.graph_objs as go
 from plotly.graph_objs import Scatter, Layout
+from datetime import date
+
 
 ROOT_URL = "http://www.ise.ie"
 
@@ -99,6 +101,81 @@ class Company:
 		history_href = company_link.get('href')		# get the href for the "history" page of the company
 		self.history_url += history_href
 
+	def date_crawler(index, start_date, end_date, companies_list):
+
+		'''
+		This function date_crawler takes in a company index, a start date DD-MM-YYY and an end date DD-MM-YYYY
+		Displays a graph of the share price for the company between the start and end dates
+		Web pages are limited to 30 results per page so it crawls through each page until it reaches the end date
+		'''
+
+		companies_list[index].get_history_href()
+
+		#http://www.ise.ie/Market-Data-Announcements/Companies/Equity-History/?ACTIVEGROUP=1&&end_day=4&start_day=1&equity=2015070&end_year=2017&start_month=1&end_month=4&start_year=2018
+
+		history_url_prefix = companies_list[index].history_url.split("equity=")[0]
+		equity_number = companies_list[index].history_url.split("equity=")[1]
+
+		active_group_num = 1
+
+		# some weird thing going on with their URL, the start and end date figures are flipped...
+		dates_url = history_url_prefix \
+		+ "ACTIVEGROUP=" + str(active_group_num) + "&" \
+		+ "&end_day=" + start_date[0] + "&start_day=" + end_date[0] \
+		+ "&equity=" + equity_number \
+		+ "&end_year=" + start_date[2] + "&start_month=" + end_date[1]\
+		+ "&end_month=" + start_date[1] + "&start_year=" + end_date[2]
+		
+		print(dates_url)
+
+		source_code = requests.get(dates_url)
+		plain_text = source_code.text
+		soup = BeautifulSoup(plain_text, "html.parser")
+
+		price_history_pages = soup.find("td", "navigationPages")	# this gets the links to the pages of share prices
+		number_pages = len(price_history_pages.findChildren())		# number_pages is the amount of pages we have to go through
+
+		print()
+
+		j = 1
+		while j <= number_pages:
+
+			if j > 1:
+				new_prefix = dates_url.split("&&")[0]
+				rest_of_date_url = dates_url.split("&&")[1]
+				new_prefix = new_prefix[:(len(new_prefix)-1)]
+				dates_url = new_prefix + str(j) + "&&" + rest_of_date_url
+				#print(dates_url)
+				source_code = requests.get(dates_url)
+				plain_text = source_code.text
+				soup = BeautifulSoup(plain_text, "html.parser")
+
+			i = 0
+
+			for row in soup.find_all('td', 'equityName'):
+				if i % 3 == 0:
+					companies_list[index].date_list.append(row.string)
+
+				elif i % 3 == 1:
+					if row.string == None:		# there are blank spaces for some share prices on the website... 
+						share_price = ""
+					else:
+						share_price = row.string[:7]
+					companies_list[index].share_price_list.append(share_price)
+
+				elif i % 3 == 2:
+					market_capital = row.string + " MIL"
+					companies_list[index].market_capital_list.append(market_capital)
+
+				i += 1
+				
+			complete_percentage = (j / number_pages)*100
+			print(str(complete_percentage) + "% Complete...")
+			j = j + 1
+
+		companies_list[index].format_graph_data()	
+		companies_list[index].plot_graph()
+
 def print_breaker():
 	print("--------------------------------------------------------------------")
 
@@ -111,3 +188,31 @@ def instructions():
 	print("<ALL> - shows every company share price sequentially")
 	print_breaker()
 	print("\n")
+
+def check_valid_date(start_date_string, end_date_string):
+
+	if 	start_date_string[2] == '//' or start_date_string[5] == '//' or \
+		end_date_string[2] == '//' or end_date_string[5] == '//' or \
+		len(start_date_string) != 11 or len(end_date_string) != 11:
+		
+		print("\nInvalid date entered...")
+		return False
+
+	f_date = date(int(start_date_string.split('/')[2].lstrip('0')), int(start_date_string.split('/')[1].lstrip('0')), int(start_date_string.split('/')[0].lstrip('0')))
+	l_date = date(int(end_date_string.split('/')[2].lstrip('0')), int(end_date_string.split('/')[1].lstrip('0')), int(end_date_string.split('/')[0].lstrip('0')))
+	delta = l_date - f_date
+	print(delta.days)
+	if delta.days <= 365:
+		return True
+	else:
+		print("Start and end date cannot be more than 1 year apart... Enter new dates")
+		return False
+
+def crawler(index):
+
+	if index == -1:
+		companies_list[index].get_history_href()
+		companies_list[index].get_share_price_history_table()		# get info from company's history webpage
+		companies_list[index].format_graph_data()	
+		companies_list[index].plot_graph()
+		time.sleep(2)		# give the program time to breathe...
--- ise_wc.py
+++ ise_wc.py
@@ -10,95 +10,6 @@ import sys
 ROOT_URL = "http://www.ise.ie"
 companies_list = {}		# list of company objects
 
-def crawler(index):
-
-	if index != -1:
-		companies_list[index].get_history_href()
-		companies_list[index].get_share_price_history_table()		# get info from company's history webpage
-		companies_list[index].format_graph_data()	
-		companies_list[index].plot_graph()
-		time.sleep(2)		# give the program time to breathe...
-
-	elif index == -1:
-		for key in companies_list:
-			companies_list[key].get_history_href()
-			companies_list[key].get_share_price_history_table()	
-			companies_list[key].format_graph_data()	
-			companies_list[key].plot_graph()
-			time.sleep(2)		# give the program time to breathe...
-
-def date_crawler(index, start_date, end_date):
-
-	'''
-	This function date_crawler takes in a company index, a start date DD-MM-YYY and an end date DD-MM-YYYY
-	Returns a graph of the share price for the company between the start and end dates
-	Web pages are limited to 30 results per page so it crawls through each page until it reaches the end date
-	'''
-
-	companies_list[index].get_history_href()
-
-	#http://www.ise.ie/Market-Data-Announcements/Companies/Equity-History/?ACTIVEGROUP=1&&end_day=1&start_day=1&equity=2015025&end_year=2017&start_month=1&end_month=1&start_year=2018
-	
-	history_url_prefix = companies_list[index].history_url.split("equity=")[0]
-	equity_number = companies_list[index].history_url.split("equity=")[1]
-
-	active_group_num = 1
-
-	dates_url = history_url_prefix \
-	+ "ACTIVEGROUP=" + str(active_group_num) + "&" \
-	+ "&end_day=" + end_date[0] + "&start_day=" + start_date[0] \
-	+ "&equity=" + equity_number \
-	+ "&end_year=" + end_date[2] + "&start_month=" + start_date[1]\
-	+ "&end_month=" + end_date[1] + "&start_year=" + start_date[2]
-
-	source_code = requests.get(dates_url)
-	plain_text = source_code.text
-	soup = BeautifulSoup(plain_text, "html.parser")
-
-	price_history_pages = soup.find("td", "navigationPages")	# this gets the links to the pages of share prices
-	number_pages = len(price_history_pages.findChildren())		# number_pages is the amount of pages we have to go through
-
-	print()
-
-	j = 1
-	while j <= number_pages:
-
-		if j > 1:
-			new_prefix = dates_url.split("&&")[0]
-			rest_of_date_url = dates_url.split("&&")[1]
-			new_prefix = new_prefix[:(len(new_prefix)-1)]
-			dates_url = new_prefix + str(j) + "&&" + rest_of_date_url
-			#print(dates_url)
-			source_code = requests.get(dates_url)
-			plain_text = source_code.text
-			soup = BeautifulSoup(plain_text, "html.parser")
-
-		i = 0
-
-		for row in soup.find_all('td', 'equityName'):
-			if i % 3 == 0:
-				companies_list[index].date_list.append(row.string)
-
-			elif i % 3 == 1:
-				if row.string == None:		# there are blank spaces for some share prices on the website... 
-					share_price = ""
-				else:
-					share_price = row.string[:7]
-				companies_list[index].share_price_list.append(share_price)
-
-			elif i % 3 == 2:
-				market_capital = row.string + " MIL"
-				companies_list[index].market_capital_list.append(market_capital)
-
-			i += 1
-			
-		complete_percentage = (j / number_pages)*100
-		print(str(complete_percentage) + "% Complete...")
-		j = j + 1
-
-	companies_list[index].format_graph_data()	
-	companies_list[index].plot_graph()
-
 def get_all_companies():
 	page = 1
 	company_index = 1
@@ -133,6 +44,7 @@ def print_company_list():
 		else:
 			print(str(companies_list[key].index) + ' - ' + companies_list[key].name)
 
+
 def main():
 	get_all_companies()
 	print("\nIrish Stock Exchange share price viewer\n")
@@ -147,22 +59,28 @@ def main():
 		user_input = sys.stdin.readline()
 
 		if user_input < "56" and user_input > "0":
-			#crawler(int(user_input))
-			start_day = "1"
-			start_month = "1"
-			start_year = "2018"
+			valid_date = False
+			while valid_date == False:
+				print("Enter a start date (DD/MM/YYYY)")
+				start_date_string = sys.stdin.readline()
+				print("Enter an end date (DD/MM/YYYY)")
+				end_date_string = sys.stdin.readline()
+				valid_date = company.check_valid_date(start_date_string, end_date_string)
 
-			end_day = "1"
-			end_month = "6"
-			end_year = "2017"
+			start_day = start_date_string.split('/')[0].lstrip('0')
+			start_month = start_date_string.split('/')[1].lstrip('0')
+			start_year = start_date_string.split('/')[2].lstrip('0')
 
-			start_date = start_day + "-" + start_month + "-" + start_year
-			end_date = end_day + "-" + end_month + "-" + end_year
+			end_day = end_date_string.split('/')[0].lstrip('0')
+			end_month = end_date_string.split('/')[1].lstrip('0')
+			end_year = end_date_string.split('/')[2].lstrip('0')
 
-			date_crawler(int(user_input), (start_day, start_month, start_year), (end_day, end_month, end_year))
+			print("Retrieving data for " + companies_list[int(user_input)].name + " for dates " + start_date_string + " - " + end_date_string)
+
+			Company.date_crawler(int(user_input), (start_day, start_month, start_year), (end_day, end_month, end_year), companies_list)
 			invalid_input = False
 		elif "<ALL>" in user_input:
-			crawler(-1)
+			company.crawler(-1)
 			invalid_input = False
 		elif "<COMPANIES>" in user_input:
 			print_company_list()
